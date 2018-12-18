9083a14a356710050b4d6f5fdd7a9963821f9867
wenghengcong <wenghengcong@icloud.com>
2018-12-19 07:12:57 +0800

    添加cf

---
+++ CoreFoundationSource/CoreFoundationSource/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CoreFoundationSource
+//
+//  Created by WengHengcong on 2018/12/18.
+//  Copyright © 2018 WengHengcong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
